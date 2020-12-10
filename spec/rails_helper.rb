93b28ec23fbc00eed2ae4fa909ebca624d7cba41
KoyoIshikawa <koyoi561@gmail.com>
2020-12-10 22:52:52 +0900

    RspecniteFakerを日本語化

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+Faker::Config.locale = :ja
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
