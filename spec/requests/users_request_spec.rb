e3330e7ff888751a8521924742d937f123a6567e
KoyoIshikawa <koyoi561@gmail.com>
2020-12-14 15:08:15 +0900

    users_controller.rbにindexの動作のテストコードを追加

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-Rspec.describe "User", type: :request do
+RSpec.describe "Users", type: :request do
+  subject { get(users_path) }
+  # ***** 以下を追加 *****
+  describe "GET #index" do
+    contxet "ユーザが存在する時" do
+      before { create_list(:user, 3)}
+      it "リクエストが成功する" do
+        subject     
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "name が表示されている" do
+        subject
+        expect(response.body).to include(*User.pluck(:name))
+      end
+    end
   
+  end
+  # ***** 以上を追加 *****
+
 end
