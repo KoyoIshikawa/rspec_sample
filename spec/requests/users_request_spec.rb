1f13c63728dc0cc5f100c9a8b4b3dbc1b1e3cd2e
KoyoIshikawa <koyoi561@gmail.com>
2020-12-14 18:35:53 +0900

    testコードを実装

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  subject { get(users_path) }
-  # ***** 以下を追加 *****
+  
   describe "GET #index" do
-    contxet "ユーザが存在する時" do
-      before { create_list(:user, 3)}
+    subject { get(users_path) }
+    context "ユーザーが存在するとき" do
+      before { create_list(:user, 3) }
       it "リクエストが成功する" do
-        subject     
+        subject
         expect(response).to have_http_status(:ok)
       end
 
@@ -18,6 +18,191 @@ RSpec.describe "Users", type: :request do
     end
   
   end
-  # ***** 以上を追加 *****
 
+  describe "GET #show" do
+    subject { get(user_path(user_id)) }
+    context "ユーザーが存在するとき" do
+      let(:user) { create(:user) }
+      let(:user_id) { user.id }
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "name が表示されている" do
+        subject
+        expect(response.body).to include user.name
+      end
+
+      it "age が表示されている" do
+        subject
+        expect(response.body).to include user.age.to_s
+
+      end
+
+      it "email が表示されている" do
+        subject
+        expect(response.body).to include user.email
+      end
+    end
+    context ":idに対応するユーザーが存在しないとき" do
+      let(:user_id) { 1 }
+
+      it "エラーが発生する" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+
+  describe "GET #new" do
+    subject { get(new_user_path) }
+    it "リクエストが成功する" do
+      subject
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET #create" do
+    context "パラメータが正常なとき" do
+      it "リクエストが成功する" do
+        post(users_path, params: { user: attributes_for(:user) })
+        expect(response).to have_http_status(302)
+      end
+
+      it "ユーザーが保存される" do
+      end
+
+      it "詳細ページにリダイレクトされる" do
+      end
+    end
+
+    context "パラメータが異常なとき" do
+      it "リクエストが成功する" do
+      end
+
+      it "ユーザーが保存されない" do
+      end
+
+      it "新規投稿ページがレンダリングされる" do
+      end
+    end
+  end
+  describe "GET #edit" do
+    subject { get(edit_user_path(user_id)) }
+    context "ユーザーが存在するとき" do
+      let(:user) { create(:user) }
+      let(:user_id) { user.id }
+
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(200)
+      end
+
+      it "name が表示されている" do
+        subject
+        expect(response.body).to include user.name
+      end
+
+      it "age が表示されている" do
+        subject
+        expect(response.body).to include user.age.to_s
+      end
+
+      it "email が表示されている" do
+        subject
+        expect(response.body).to include user.email
+      end
+    end
+
+    # ***** 以下のテストは任意 *****
+    context ":idに対応するユーザーが存在しないとき" do
+      let(:user_id) { 1 }
+
+      it "エラーが発生する" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+    # ***** 以上のテストは任意 *****
+  end
+  describe 'GET #update' do
+    subject { patch(user_path(user.id), params: params) }
+    let(:user) { create(:user) }
+
+    context 'パラメータが正常な場合' do
+      let(:params) { { user: attributes_for(:user) } }
+
+      it 'リクエストが成功する' do
+        subject
+        expect(response).to have_http_status(302)
+      end
+
+      it 'name が更新される' do
+        origin_name = user.name
+        new_name = params[:user][:name]
+        expect { subject }.to change { user.reload.name }.from(origin_name).to(new_name)
+      end
+
+      it 'age が更新される' do
+        origin_age = user.age
+        new_age = params[:user][:age]
+        expect { subject }.to change { user.reload.age }.from(origin_age).to(new_age)
+      end
+
+      it 'email が更新される' do
+        origin_email = user.email
+        new_email = params[:user][:email]
+        expect { subject }.to change { user.reload.email }.from(origin_email).to(new_email)
+      end
+
+      it '詳細ページにリダイレクトされる' do
+        subject
+        expect(response).to redirect_to User.last
+      end
+    end
+    context 'user のパラメータが異常なとき' do
+      let(:params) { { user: attributes_for(:user, :invalid) } }
+
+      it 'リクエストが成功する' do
+        subject
+        expect(response).to have_http_status(200)
+      end
+
+      it 'name が更新されない' do
+        expect { subject }.not_to change(user.reload, :name)
+      end
+
+      it 'age が更新されない' do
+        expect { subject }.not_to change(user.reload, :age)
+      end
+
+      it 'email が更新されない' do
+        expect { subject }.not_to change(user.reload, :email)
+      end
+
+      it '編集ページがレンダリングされる' do
+        subject
+        expect(response.body).to include '編集'
+      end
+    end
+  end
+  describe 'GET #destroy' do
+    subject { delete(user_path(user.id)) }
+    let!(:user) { create(:user) }
+
+    context 'パラメータが正常な場合' do
+      it 'リクエストが成功する' do
+        subject
+        expect(response).to have_http_status(302)
+      end
+
+      it 'ユーザーが削除される' do
+        expect { subject }.to change(User, :count).by(-1)
+      end
+
+      it 'ユーザー一覧にリダイレクトすること' do
+        subject
+        expect(response).to redirect_to(users_path)
+      end
+    end
+  end
 end
